a7707f0065cfb5eb69cf630b50c77740bfb900d3
Thiago William <thiago.will.silva@gmail.com>
2020-09-11 14:55:18 -0300

    Script appointment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+const buttonConsulta = document.querySelector('.choiceConsulta');
+const buttonExame = document.querySelector('.choiceExame');
+
+const textTypeAppointment = document.querySelector('.typeAppointment');
+
+const inputConsulta = document.querySelector('.inputConsulta');
+const inputExame = document.querySelector('.inputExame');
+
+buttonConsulta.addEventListener('click', function (ev) {
+  ev.preventDefault();
+  textTypeAppointment.innerHTML = 'Consulta';
+  inputExame.style.display = 'none';
+  inputConsulta.style.display = 'block';
+  if (buttonExame.classList.contains('hasBlue')) {
+    buttonExame.classList.remove('hasBlue');
+    buttonConsulta.classList.add('hasBlue');
+  }
+});
+
+buttonExame.addEventListener('click', function (ev) {
+  ev.preventDefault();
+  textTypeAppointment.innerHTML = 'Exame';
+  inputExame.style.display = 'block';
+  inputConsulta.style.display = 'none';
+  if (buttonConsulta.classList.contains('hasBlue')) {
+    buttonConsulta.classList.remove('hasBlue');
+    buttonExame.classList.add('hasBlue');
+  }
+});
+
+//Acordian perguntas frequentes
 var acc = document.getElementsByClassName('accordion');
 var i;
 
