a37780ce90f5ed2144d3d42b1dffe7ec22b9c7e5
Thiago William <thiago.will.silva@gmail.com>
2020-09-15 08:53:49 -0300

    ajustes responsivo

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ var acc = document.getElementsByClassName('accordion');
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener('click', () => {
+  acc[i].addEventListener('click', function () {
     this.classList.toggle('active');
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
